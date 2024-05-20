da47479e71e6da8f68805a36a9c56cc7b642bfab
NandanTyagi <nandantyagi@gmail.com>
2024-05-21 01:23:08 +0200

    Fix footer2

--- components/v1/StandardButton.jsx
+++ components/v1/StandardButton.jsx
@@ -128,7 +128,7 @@ const StandardButton = ({ text, type = "pill" }) => {
       <button
         onClick={() => handleClick(buttonRef.current.id)}
         title={"Try free"}
-        className={`group relative m-1 inline-flex cursor-pointer w-[100%]  items-center justify-center overflow-hidden rounded-full border-b-2 border-l-2 border-r-2 border-[#330594] bg-gradient-to-tr py-6 text-white shadow-lg transition duration-100 ease-in-out active:translate-y-0.5 active:border-red-600 active:shadow-none from-[#330594] to-[#330594] text-center`}
+        className={`group relative m-1 inline-flex cursor-pointer w-[100%]  items-center justify-center overflow-hidden rounded-full border-b-2 border-l-2 border-r-2 border-[#330594] bg-gradient-to-tr py-4 text-white shadow-lg transition duration-100 ease-in-out active:translate-y-0.5 active:border-red-600 active:shadow-none from-[#330594] to-[#330594] text-center`}
         ref={buttonRef}
         id={text}
       >
