7ed0e3cdf981d5203bd5a855cadd983a765d0514
NandanTyagi <nandantyagi@gmail.com>
2024-05-21 23:28:51 +0200

    Fix mobile save button text

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -148,16 +148,20 @@ const Nav = ({ isNav = true }) => {
             </ThreeDButton>
           </div>
           <button
-            className={`block sm:hidden w-[16px] ${
-              !isSaved
-                ? "outline-dotted outline-red-500 bg-red-500 p-[2px] rounded"
-                : null
-            }`}
+            className={`block sm:hidden w-[12px] pt-1`}
             onClick={handelCtxMenu}
           >
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+          {isSaved ? <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
               <path d="M48 96V416c0 8.8 7.2 16 16 16H384c8.8 0 16-7.2 16-16V170.5c0-4.2-1.7-8.3-4.7-11.3l33.9-33.9c12 12 18.7 28.3 18.7 45.3V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96C0 60.7 28.7 32 64 32H309.5c17 0 33.3 6.7 45.3 18.7l74.5 74.5-33.9 33.9L320.8 84.7c-.3-.3-.5-.5-.8-.8V184c0 13.3-10.7 24-24 24H104c-13.3 0-24-10.7-24-24V80H64c-8.8 0-16 7.2-16 16zm80-16v80H272V80H128zm32 240a64 64 0 1 1 128 0 64 64 0 1 1 -128 0z" />
-            </svg>
+            </svg> : <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+              <path d="M48 96V416c0 8.8 7.2 16 16 16H384c8.8 0 16-7.2 16-16V170.5c0-4.2-1.7-8.3-4.7-11.3l33.9-33.9c12 12 18.7 28.3 18.7 45.3V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96C0 60.7 28.7 32 64 32H309.5c17 0 33.3 6.7 45.3 18.7l74.5 74.5-33.9 33.9L320.8 84.7c-.3-.3-.5-.5-.8-.8V184c0 13.3-10.7 24-24 24H104c-13.3 0-24-10.7-24-24V80H64c-8.8 0-16 7.2-16 16zm80-16v80H272V80H128zm32 240a64 64 0 1 1 128 0 64 64 0 1 1 -128 0z" fill="red" />
+            </svg>}
+            
+            <div className={`text-[0.7rem] ml-[-60%] mt-[-2px] ${
+              !isSaved
+                ? " text-red-500 font-bold"
+                : "font-light"
+            }`}>Save</div>
           </button>
         </div>
       </div>
