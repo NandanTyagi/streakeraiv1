17d76d1e3c16dc1e8fa9db6c4a61e3c0314f892c
NandanTyagi <nandantyagi@gmail.com>
2024-10-04 21:58:03 +0200

    Refactor Hero component to use different images based on screen size

--- components/v1/Hero.jsx
+++ components/v1/Hero.jsx
@@ -5,7 +5,8 @@ const Hero = () => {
     <div className="">
       <div className="absolute z-[10] inset-0 bg-black opacity-60"></div>
 
-      <Image src="/streaker-mobile-bg-2.png" alt="logo" priority fill />
+      <Image className="md:hidden" src="/mobile-fade.webp" alt="logo" priority fill />
+      <Image className="hidden md:block" src="/streaker-bg-landscape-fade.webp" alt="logo" priority fill />
 
       <h1 className="absolute z-[10] bottom-[80px] sm:bottom-[80px] text-[2.6rem] pb-2 sm:text-6xl font-semibold text-center text-white w-[100%] font-lato tracking-[4px]">
         Streaker.ai
