6a974fe231973341d320690d2f7a0226e1704cb7
NandanTyagi <nandantyagi@gmail.com>
2024-11-14 22:40:20 +0100

    Remove unused themeColor and icon properties from metadata in layout component

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,12 +12,6 @@ export const metadata = {
   openGraph: {
     images: "/opengraph-image.png",
   },
-  themeColor: '#ff5722',
-  icons: {
-    icon: '/streaker-logo-192x192.png',
-    apple: '/streaker-logo-180x180.png',
-  },
-  manifest: '/manifest.json',
 };
 
 export default function RootLayout({ children }) {
