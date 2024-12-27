a6528ada1fcdea79ed652d565054c1a52adf9274
NandanTyagi <nandantyagi@gmail.com>
2024-12-27 09:58:25 +0100

    Update PWA configuration to disable service worker in production environment and add service worker source map

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const withPWA = require('next-pwa')({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
+  disable: process.env.NODE_ENV === "production",
 });
 
 const nextConfig = {
