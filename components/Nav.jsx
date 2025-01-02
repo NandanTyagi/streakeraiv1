56702cd313f83c19e6c59345cb7bcc54b2d8b561
NandanTyagi <nandantyagi@gmail.com>
2025-01-02 22:51:22 +0100

    Add conditional rendering for ThreeDButton in Nav component based on history state

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,8 +8,10 @@ import { AppContext } from "@/context/appContext";
 import savePanelToDb from "@/utils/v2/savePanelToDb";
 import ThreeDButton from "@/components/ui/button/3DButton";
 import { useRouter } from "next/navigation";
-import { useSearchParams } from "next/navigation";
+import { useSearchParams, useParams, usePathname } from "next/navigation";
+
 import { useToast } from "@/hooks/use-toast";
+import { DashboardIcon } from "@radix-ui/react-icons";
 
 import {
   Dialog,
@@ -26,6 +28,8 @@ const Nav = ({ isNav = true, isHistory }) => {
   const { user } = useKindeBrowserClient();
   const router = useRouter();
   const searchParams = useSearchParams();
+  const params = useParams();
+  const pathname = usePathname();
   const [isAppLoading, setisAppLoading] = useState(false);
   const [showPanelSavedDialog, setShowPanelSavedDialog] = useState(false);
   const { board, setBoard, isSaved, setIsSaved } = useContext(AppContext);
@@ -248,26 +252,50 @@ const Nav = ({ isNav = true, isHistory }) => {
             isHistory={isHistory}
           />
         )}
-        <div className={`absolute right-6 ${isHistory ? "hidden" : null}`}>
+        <div className={`absolute right-6 ${isHistory ? null : null}`}>
           <div
             className={`hidden md:block ${
-              !isSaved ? "border-red-500 border-2 rounded-lg" : null
+              !isSaved && !isHistory
+                ? "border-red-500 border-2 rounded-lg"
+                : null
             }`}
           >
-            <ThreeDButton
-              isSaved={isSaved}
-              text="Save"
-              onClick={handelCtxMenu}
-              title="save"
-            >
-              {" "}
-              <span className="w-[80%] font-semibold">Save</span>
-              <span className="w-[20%] flex justify-center items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                  <path d="M48 96V416c0 8.8 7.2 16 16 16H384c8.8 0 16-7.2 16-16V170.5c0-4.2-1.7-8.3-4.7-11.3l33.9-33.9c12 12 18.7 28.3 18.7 45.3V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96C0 60.7 28.7 32 64 32H309.5c17 0 33.3 6.7 45.3 18.7l74.5 74.5-33.9 33.9L320.8 84.7c-.3-.3-.5-.5-.8-.8V184c0 13.3-10.7 24-24 24H104c-13.3 0-24-10.7-24-24V80H64c-8.8 0-16 7.2-16 16zm80-16v80H272V80H128zm32 240a64 64 0 1 1 128 0 64 64 0 1 1 -128 0z" />
-                </svg>
-              </span>
-            </ThreeDButton>
+            {isHistory ? (
+              <ThreeDButton
+                isSaved={isSaved}
+                text="Dashboard"
+                onClick={() =>
+                  router.push(`${location.origin}/${pathname}/dashboard?${searchParams.toString()}`)
+                }
+                title="Dashboard"
+                isHistory={isHistory}
+              >
+                {" "}
+                <span className="w-[80%] font-semibold"></span>
+                <span className="w-[20%] flex justify-center items-center">
+                  <DashboardIcon
+                    width={48}
+                    height={48}
+                    className="text-white rotate-90"
+                  />
+                </span>
+              </ThreeDButton>
+            ) : (
+              <ThreeDButton
+                isSaved={isSaved}
+                text="Save"
+                onClick={handelCtxMenu}
+                title="save"
+              >
+                {" "}
+                <span className="w-[80%] font-semibold">Save</span>
+                <span className="w-[20%] flex justify-center items-center">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                    <path d="M48 96V416c0 8.8 7.2 16 16 16H384c8.8 0 16-7.2 16-16V170.5c0-4.2-1.7-8.3-4.7-11.3l33.9-33.9c12 12 18.7 28.3 18.7 45.3V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96C0 60.7 28.7 32 64 32H309.5c17 0 33.3 6.7 45.3 18.7l74.5 74.5-33.9 33.9L320.8 84.7c-.3-.3-.5-.5-.8-.8V184c0 13.3-10.7 24-24 24H104c-13.3 0-24-10.7-24-24V80H64c-8.8 0-16 7.2-16 16zm80-16v80H272V80H128zm32 240a64 64 0 1 1 128 0 64 64 0 1 1 -128 0z" />
+                  </svg>
+                </span>
+              </ThreeDButton>
+            )}
           </div>
           <button
             className={`flex flex-col md:hidden w-[14px] pt-1 ${
