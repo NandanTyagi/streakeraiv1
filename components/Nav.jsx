3fdb81d33e7109e872398347d2b4ecedeb3c3efd
NandanTyagi <nandantyagi@gmail.com>
2025-01-03 00:03:55 +0100

    Enhance Nav component to conditionally display Dashboard and Save buttons based on history state; improve unsaved changes warning logic

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -173,7 +173,7 @@ const Nav = ({ isNav = true, isHistory }) => {
 
   useEffect(() => {
     const handleBeforeUnload = (event) => {
-      if (!isSaved) {
+      if (!isSaved && !isHistory) {
         const message =
           "You have unsaved changes. Are you sure you want to leave?";
         event.returnValue = message; // Standard for most browsers
@@ -250,101 +250,106 @@ const Nav = ({ isNav = true, isHistory }) => {
             isHistory={isHistory}
           />
         )}
-        <div className={`absolute right-6`}>
-          <div
-            className={`hidden md:block`}
-          >
-            <ThreeDButton
-              isSaved={isSaved}
-              text="Dashboard"
-              onClick={() => router.push(`${location.origin}/${pathname}/dashboard/${searchParams.toString()}`)}
-              title="dashboard"
-              isHistory={isHistory}
+        {isHistory ? (
+          <div className={`absolute right-6`}>
+            <div className={`hidden md:block`}>
+              <ThreeDButton
+                isSaved={isSaved}
+                text="Dashboard"
+                onClick={() =>
+                  router.push(
+                    `${
+                      location.origin
+                    }/${pathname}/dashboard/${searchParams.toString()}`
+                  )
+                }
+                title="dashboard"
+                isHistory={isHistory}
+              >
+                {" "}
+                <span className="w-[80%] font-semibold">Dash</span>
+                <span className="w-[20%] flex justify-center items-center">
+                  <DashboardIcon
+                    width={20}
+                    height={20}
+                    className="text-black rotate-90"
+                  />
+                </span>
+              </ThreeDButton>
+            </div>
+            <button
+              className={`flex flex-col md:hidden w-[14px] pt-1`}
+              onClick={() =>
+                router.push(
+                  `${
+                    location.origin
+                  }/${pathname}/dashboard/${searchParams.toString()}`
+                )
+              }
             >
-              {" "}
-              <span className="w-[80%] font-semibold">Dash</span>
-              <span className="w-[20%] flex justify-center items-center">
-              <DashboardIcon width={20} height={20} className="text-black rotate-90" />
+              <DashboardIcon
+                width={16}
+                height={16}
+                className="text-black rotate-90"
+              />
+              <span
+                className={`text-[0.7rem] md:text-[0.7rem] ml-[-6px] mt-[-2px]`}
+              >
+                Dash
               </span>
-            </ThreeDButton>
+            </button>
           </div>
-          <button
-            className={`flex flex-col md:hidden w-[14px] pt-1 ${
-              isHistory ? "hidden" : null
-            }`}
-            onClick={handelCtxMenu}
-          >
-            {isSaved ? (
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                <path d="M48 96V416c0 8.8 7.2 16 16 16H384c8.8 0 16-7.2 16-16V170.5c0-4.2-1.7-8.3-4.7-11.3l33.9-33.9c12 12 18.7 28.3 18.7 45.3V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96C0 60.7 28.7 32 64 32H309.5c17 0 33.3 6.7 45.3 18.7l74.5 74.5-33.9 33.9L320.8 84.7c-.3-.3-.5-.5-.8-.8V184c0 13.3-10.7 24-24 24H104c-13.3 0-24-10.7-24-24V80H64c-8.8 0-16 7.2-16 16zm80-16v80H272V80H128zm32 240a64 64 0 1 1 128 0 64 64 0 1 1 -128 0z" />
-              </svg>
-            ) : (
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                <path
-                  d="M48 96V416c0 8.8 7.2 16 16 16H384c8.8 0 16-7.2 16-16V170.5c0-4.2-1.7-8.3-4.7-11.3l33.9-33.9c12 12 18.7 28.3 18.7 45.3V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96C0 60.7 28.7 32 64 32H309.5c17 0 33.3 6.7 45.3 18.7l74.5 74.5-33.9 33.9L320.8 84.7c-.3-.3-.5-.5-.8-.8V184c0 13.3-10.7 24-24 24H104c-13.3 0-24-10.7-24-24V80H64c-8.8 0-16 7.2-16 16zm80-16v80H272V80H128zm32 240a64 64 0 1 1 128 0 64 64 0 1 1 -128 0z"
-                  fill="red"
-                />
-              </svg>
-            )}
-
-            <span
-              className={`text-[0.7rem] md:text-[0.7rem] ml-[-6px] mt-[-2px] ${
-                !isSaved ? " text-red-500 font-bold" : "font-light"
+        ) : (
+          <div className={`absolute right-6 ${isHistory ? "hidden" : null}`}>
+            <div
+              className={`hidden md:block ${
+                !isSaved ? "border-red-500 border-2 rounded-lg" : null
               }`}
             >
-              Dash
-            </span>
-          </button>
-        </div>
-        <div className={`absolute right-6 ${isHistory ? "hidden" : null}`}>
-          <div
-            className={`hidden md:block ${
-              !isSaved ? "border-red-500 border-2 rounded-lg" : null
-            }`}
-          >
-            <ThreeDButton
-              isSaved={isSaved}
-              text="Save"
+              <ThreeDButton
+                isSaved={isSaved}
+                text="Save"
+                onClick={handelCtxMenu}
+                title="save"
+              >
+                {" "}
+                <span className="w-[80%] font-semibold">Save</span>
+                <span className="w-[20%] flex justify-center items-center">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                    <path d="M48 96V416c0 8.8 7.2 16 16 16H384c8.8 0 16-7.2 16-16V170.5c0-4.2-1.7-8.3-4.7-11.3l33.9-33.9c12 12 18.7 28.3 18.7 45.3V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96C0 60.7 28.7 32 64 32H309.5c17 0 33.3 6.7 45.3 18.7l74.5 74.5-33.9 33.9L320.8 84.7c-.3-.3-.5-.5-.8-.8V184c0 13.3-10.7 24-24 24H104c-13.3 0-24-10.7-24-24V80H64c-8.8 0-16 7.2-16 16zm80-16v80H272V80H128zm32 240a64 64 0 1 1 128 0 64 64 0 1 1 -128 0z" />
+                  </svg>
+                </span>
+              </ThreeDButton>
+            </div>
+            <button
+              className={`flex flex-col md:hidden w-[14px] pt-1 ${
+                isHistory ? "hidden" : null
+              }`}
               onClick={handelCtxMenu}
-              title="save"
             >
-              {" "}
-              <span className="w-[80%] font-semibold">Save</span>
-              <span className="w-[20%] flex justify-center items-center">
+              {isSaved ? (
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                   <path d="M48 96V416c0 8.8 7.2 16 16 16H384c8.8 0 16-7.2 16-16V170.5c0-4.2-1.7-8.3-4.7-11.3l33.9-33.9c12 12 18.7 28.3 18.7 45.3V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96C0 60.7 28.7 32 64 32H309.5c17 0 33.3 6.7 45.3 18.7l74.5 74.5-33.9 33.9L320.8 84.7c-.3-.3-.5-.5-.8-.8V184c0 13.3-10.7 24-24 24H104c-13.3 0-24-10.7-24-24V80H64c-8.8 0-16 7.2-16 16zm80-16v80H272V80H128zm32 240a64 64 0 1 1 128 0 64 64 0 1 1 -128 0z" />
                 </svg>
-              </span>
-            </ThreeDButton>
-          </div>
-          <button
-            className={`flex flex-col md:hidden w-[14px] pt-1 ${
-              isHistory ? "hidden" : null
-            }`}
-            onClick={handelCtxMenu}
-          >
-            {isSaved ? (
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                <path d="M48 96V416c0 8.8 7.2 16 16 16H384c8.8 0 16-7.2 16-16V170.5c0-4.2-1.7-8.3-4.7-11.3l33.9-33.9c12 12 18.7 28.3 18.7 45.3V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96C0 60.7 28.7 32 64 32H309.5c17 0 33.3 6.7 45.3 18.7l74.5 74.5-33.9 33.9L320.8 84.7c-.3-.3-.5-.5-.8-.8V184c0 13.3-10.7 24-24 24H104c-13.3 0-24-10.7-24-24V80H64c-8.8 0-16 7.2-16 16zm80-16v80H272V80H128zm32 240a64 64 0 1 1 128 0 64 64 0 1 1 -128 0z" />
-              </svg>
-            ) : (
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                <path
-                  d="M48 96V416c0 8.8 7.2 16 16 16H384c8.8 0 16-7.2 16-16V170.5c0-4.2-1.7-8.3-4.7-11.3l33.9-33.9c12 12 18.7 28.3 18.7 45.3V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96C0 60.7 28.7 32 64 32H309.5c17 0 33.3 6.7 45.3 18.7l74.5 74.5-33.9 33.9L320.8 84.7c-.3-.3-.5-.5-.8-.8V184c0 13.3-10.7 24-24 24H104c-13.3 0-24-10.7-24-24V80H64c-8.8 0-16 7.2-16 16zm80-16v80H272V80H128zm32 240a64 64 0 1 1 128 0 64 64 0 1 1 -128 0z"
-                  fill="red"
-                />
-              </svg>
-            )}
+              ) : (
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                  <path
+                    d="M48 96V416c0 8.8 7.2 16 16 16H384c8.8 0 16-7.2 16-16V170.5c0-4.2-1.7-8.3-4.7-11.3l33.9-33.9c12 12 18.7 28.3 18.7 45.3V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96C0 60.7 28.7 32 64 32H309.5c17 0 33.3 6.7 45.3 18.7l74.5 74.5-33.9 33.9L320.8 84.7c-.3-.3-.5-.5-.8-.8V184c0 13.3-10.7 24-24 24H104c-13.3 0-24-10.7-24-24V80H64c-8.8 0-16 7.2-16 16zm80-16v80H272V80H128zm32 240a64 64 0 1 1 128 0 64 64 0 1 1 -128 0z"
+                    fill="red"
+                  />
+                </svg>
+              )}
 
-            <span
-              className={`text-[0.7rem] md:text-[0.7rem] ml-[-6px] mt-[-2px] ${
-                !isSaved ? " text-red-500 font-bold" : "font-light"
-              }`}
-            >
-              Save
-            </span>
-          </button>
-        </div>
+              <span
+                className={`text-[0.7rem] md:text-[0.7rem] ml-[-6px] mt-[-2px] ${
+                  !isSaved ? " text-red-500 font-bold" : "font-light"
+                }`}
+              >
+                Save
+              </span>
+            </button>
+          </div>
+        )}
         {confirmOpen && (
           <Dialog open={confirmOpen} onOpenChange={setConfirmOpen}>
             <DialogContent className="bg-white">
